8c3116e5ed361686572b7e3455fa045051592922
phunksbot <86929652+phunksbot@users.noreply.github.com>
2021-08-18 19:01:51 +0200

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -14,7 +14,7 @@ const twitterClient = new twit(twitterConfig);
 
 // OpenSea doesn't give us access to Webhooks; need to poll every 60 seconds
 // Occasionaly in the split second of delay, dupelicates are retrieved - filter them out here
-async function handleDupesAndTweet(tokenName, tweetText, imageUrl) {
+async function handleDupesAndTweet(tokenName, tweetText) {
     // Search our twitter account's recent tweets for anything exactly matching our new tweet's text
     twitterClient.get('search/tweets', { q: tokenName, count: 1, result_type: 'recent' }, (error, data, response) => {
         if (!error) {
@@ -34,38 +34,12 @@ async function handleDupesAndTweet(tokenName, tweetText, imageUrl) {
             if (statusOlderThan10Mins) {
                 console.log('Previous status is older than 10 minutes, continuing to tweet...');
 
-                return tweet(tweetText, imageUrl);
+                return tweet(tweetText);
             }
 
             console.error('Tweet is a duplicate; possible delayed transaction retrieved from OpenSea');
         } else {
             console.error(err);
-        }
-    });
-}
-
-// Upload image of item retrieved from OpenSea & then tweet that image + provided text
-async function tweet(tweetText, imageUrl) {
-    // Format our image to base64
-    const processedImage = await getBase64(imageUrl);
-
-    // Upload the item's image from OpenSea to Twitter & retrieve a reference to it
-    twitterClient.post('media/upload', { media_data: processedImage }, (error, media, response) => {
-        if (!error) {
-            const tweet = {
-                status: tweetText,
-                media_ids: [media.media_id_string]
-            };
-
-            twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
-                if (!error) {
-                    console.log(`Successfully tweeted: ${tweetText}`);
-                } else {
-                    console.error(error);
-                }
-            });
-        } else {
-            console.error(error);
         }
     });
 }
